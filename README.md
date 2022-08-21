b7e7ff1739a7de3c65fe647703eba9bb9d622c19
Santiago Padilla <santiagopadilla117@gmail.com>
2022-08-21 17:14:31 -0500

    readme and env added-fixe

---
+++ README.md
@@ -0,0 +1,15 @@
+### Portfolio 
+
+## Portfolio Repository
+
+1. Clone repository
+2. npm install
+3. change variables of the .env file
+4. customize to your liking
+
+
+# commands
+
+1. npm run dev
+2. npm run build
+3. npm run preview
--- src/helpers/getEnvVariables.ts
+++ src/helpers/getEnvVariables.ts
@@ -1,9 +1,11 @@
 
 export const getEnvVariables = () => {
     
-    import.meta.env;
+    // import.meta.env;
 
     return {
-        ...import.meta.env
+        VITE_EMAIL_PUBLIC_KEY: import.meta.env.VITE_EMAIL_PUBLIC_KEY,
+        VITE_SERVICE_ID: import.meta.env.VITE_SERVICE_ID,
+        VITE_TEMPLATE_ID:import.meta.env.VITE_TEMPLATE_ID,
     }
 }
